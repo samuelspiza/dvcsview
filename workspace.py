adc6e921ea6964176063d6b54986efafafacacac
Samuel Spiza <samuelspiza@gmail.com>
2009-11-25 20:48:12 +0100

    Changed git status check.

--- workspace.py
+++ workspace.py
@@ -9,7 +9,7 @@ def workspace():
     for w in workspaces:
         repos.extend(findrepos(w))
     for repo in repos:
-        print repo
+        print repo.statusstring
 
 def findrepos(path):
     entries = os.listdir(path)
@@ -43,16 +43,18 @@ class Git():
         return config
 
     def buildStatusString(self):
-        status = os.popen("git status").read()
-        reg = r'nothing to commit'
-        matchobj = re.search(reg, status)
-        if matchobj is None:
-            return "NOT CLEAR " + self.path
+        status = [l.strip() for l in os.popen("git status").readlines()]
+        if status[-1][:17] != "nothing to commit":
+            statusstring = "NOT CLEAN " + self.path
         else:
-            return "CLEAR " + self.path
+            statusstring = "CLEAN " + self.path
+        if 2 < len(status):
+            warnings = ["  " + s for s in status[1:-2] if 0 < len(s[1:].strip())]
+            statusstring = statusstring + "\n" + "\n".join(warnings[:3])
+        return statusstring
 
     def __str__(self):
-        return self.statusstring
+        return self.path
 
 
 if __name__ == "__main__":
