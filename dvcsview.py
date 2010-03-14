89fb9fd4cc95564aaf1c60f5abaf3fa6e149bebc
Samuel Spiza <samuelspiza@gmail.com>
2010-03-14 15:00:56 +0100

    Added unlicense

--- dvcsview.py
+++ dvcsview.py
@@ -1,7 +1,31 @@
 #!/usr/bin/env python
-
-"""
-dvcsview - prints status summary for DVCS repositories
+#
+# This is free and unencumbered software released into the public domain.
+# 
+# Anyone is free to copy, modify, publish, use, compile, sell, or
+# distribute this software, either in source code form or as a compiled
+# binary, for any purpose, commercial or non-commercial, and by any
+# means.
+# 
+# In jurisdictions that recognize copyright laws, the author or authors
+# of this software dedicate any and all copyright interest in the
+# software to the public domain. We make this dedication for the benefit
+# of the public at large and to the detriment of our heirs and
+# successors. We intend this dedication to be an overt act of
+# relinquishment in perpetuity of all present and future rights to this
+# software under copyright law.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+# 
+# For more information, please refer to <http://unlicense.org/>
+#
+"""dvcsview - prints status summary for DVCS repositories
 
 This tool helps to get an overview of the status of Git and Mercurial
 repositories. The script searches for all repos in your workspaces and prints
@@ -30,14 +54,14 @@ http://gist.github.com/258034
 import sys, os, ConfigParser, optparse, re
 from subprocess import call, Popen, PIPE
 
-CONFIGFILES = [os.path.expanduser("~/.dvcsview.conf"), ".dvcsview.conf"]
+CONFIG_FILES = [os.path.expanduser("~/.dvcsview.conf"), ".dvcsview.conf"]
 WORKSPACES = "workspaces"
 REPOS = "repos"
 FETCH = "fetch"
 
 def main(argv):
     config = ConfigParser.ConfigParser()
-    config.read(CONFIGFILES)
+    config.read(CONFIG_FILES)
 
     options = getOptions(argv)
 
